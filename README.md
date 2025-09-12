7bcf82a0ea4e433d97551507552d50e7af728e5a
Renee Desporte <desporterenee@gmail.com>
2025-09-11 17:52:41 -0700

    Functional experiment.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Experience the principles of [Active Noise Control](https://en.wikipedia.org/wiki/Active_noise_control) firsthand! This codebase takes a 2-channel capable output device — e.g., your laptop speakers — and plays a signal which will destructively interefere at some point in space.
 
 Check out my blog post on this experiment, and try it out yourself: find the "dead zone" based on your speaker configuration.
+
+Run the code with `uv run main.py` to find your audio devices. Add the `-d` flag when you're ready to specify an output device, e.g., `uv run main.py -d 1`.
 ## Dependencies
 ### Code
 Code dependencies are managed with uv.
---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+import argparse
+import sounddevice as sd
+import numpy as np
+
+
+start_idx = 0
+
+def main():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        '-d', '--device', default=None, type=int,
+        help='Output device.'
+    )
+    args = parser.parse_args()
+    if args.device is None:
+        print(sd.query_devices())
+        exit()
+    if sd.query_devices(args.device)["max_output_channels"] < 2:
+        raise RuntimeError(
+            f"Audio device incompatible: `{sd.query_devices(args.device)['name']}`."
+        )
+
+    try:
+        FREQUENCY = 440  # Hz (4th octave A)
+        samplerate = sd.query_devices(args.device, 'output')['default_samplerate']
+
+        def callback(outdata, frames, time, status):
+            if status:
+                print(status, file=sys.stderr)
+            global start_idx
+            t = (start_idx + np.arange(frames)) / samplerate
+            t = t.reshape(-1, 1)
+            # 180 degrees out of phase
+            mono_left = np.sin(2 * np.pi * FREQUENCY * t)
+            mono_right = np.sin(np.pi * FREQUENCY * t)
+            stereo_data = np.column_stack((mono_left, mono_right))
+            outdata[:] = stereo_data
+            start_idx += frames
+
+        with sd.OutputStream(device=1, channels=2, callback=callback,
+                            samplerate=samplerate):
+            print('#' * 80)
+            print('Press Return to quit!')
+            print('#' * 80)
+            input()
+    except KeyboardInterrupt:
+        print("Stopping due to KeyboardInterrupt!")
+        exit()
+    except Exception as e:
+        print(f"Other exception: {e}")
+
+if __name__ == "__main__":
+    main()
